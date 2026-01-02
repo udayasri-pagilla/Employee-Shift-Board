2f2beaa42e8de911d0ace08d62cd1d8aa796bfef
udayasri-pagilla <udayasripagilla1873@gmail.com>
2026-01-02 20:01:21 +0530

    db.js is updated

--- employee-shift-board-backend/src/config/db.js
+++ employee-shift-board-backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log("MongoDB connected to:", mongoose.connection.name);
   } catch (err) {
     console.error(err);
     process.exit(1);
