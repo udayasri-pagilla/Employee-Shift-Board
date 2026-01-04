87f8500cc5af0daa31a1523825367191745bed48
udayasri-pagilla <udayasripagilla1873@gmail.com>
2026-01-04 11:28:39 +0530

    Fix: use production backend URL

--- employee-shift-board-frontend/src/api/api.js
+++ employee-shift-board-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://employee-shift-board-fp44.onrender.com/api"
 });
 
 export const login = (data) => api.post("/login", data);
