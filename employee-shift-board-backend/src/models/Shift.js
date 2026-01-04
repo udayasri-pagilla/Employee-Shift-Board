321add59981028a66c4e3eddd31f617417b63ddb
udayasri-pagilla <udayasripagilla1873@gmail.com>
2026-01-04 14:24:29 +0530

    model reference updated

--- employee-shift-board-backend/src/models/Shift.js
+++ employee-shift-board-backend/src/models/Shift.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const shiftSchema = new mongoose.Schema({
-  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: "  Employee" },
+  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee" },
   date: { type: String, required: true },
   startTime: String,
   endTime: String
